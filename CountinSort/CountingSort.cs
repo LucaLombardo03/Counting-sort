5522f81c46accca4b63379a98dac18e2d8182ae0
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2020-02-24 09:48:37 +0100

    Add project files.

---
+++ CountinSort/CountingSort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Counting_Sort
+{
+    public class CountingSort
+    {
+        public static int[] Counting_Sort(int[]array)
+        {
+            int[] sortedArray = new int[array.Length];
+            int[] minori = new int[array.Length];
+            
+            for(int i = 0; i < array.Length; i++)
+            {
+                int c = 0;
+                for (int j = 0; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        c++;
+                    }
+                }
+                minori[i] = c;
+            }
+            for(int i = 0; i < array.Length; i++)
+            {
+                sortedArray[minori[i]] = array[i];
+            }
+            return sortedArray;
+        }
+    }
+}
+
+
---
+++ OrdinamentoCountingSort/Program.cs
@@ -0,0 +1,39 @@
+﻿using Counting_Sort;
+using System;
+using System.Collections.Generic;
+
+namespace OrdinamentoCountingSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int TANTI = 20;
+            Random r = new Random();
+            int[] array = new int[TANTI];
+            List<int> numeri = new List<int>();
+            for (int i = 0; i < TANTI; i++)
+            {
+                int k;
+                do
+                {
+                    k = r.Next(0, 100);
+                } while (numeri.Contains(k));
+                numeri.Add(k);
+                array[i] = k;
+            }
+            Console.WriteLine("STAMPO ARRAY NON ORDINATO: ");
+            for(int i = 0; i< array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+            int[] sortedArray = CountingSort.Counting_Sort(array);
+            Console.WriteLine("STAMPO ARRAY ORDINATO: ");
+            for (int i = 0; i < TANTI; i++)
+            {
+                Console.Write(sortedArray[i] + " ");
+            }
+        }
+    }
+}
